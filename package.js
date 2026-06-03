efbb045d74c611e66eefcdcbe42a3018b38bd52a
vasilikix5 <xafivasiliki@gmail.com>
2026-06-03 10:54:44 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "tic-tac-toe-online",
+  "version": "1.0.0",
+  "description": "Real-time Turn Based Tic-Tac-Toe",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "socket.io": "^4.7.5"
+  }
+}
